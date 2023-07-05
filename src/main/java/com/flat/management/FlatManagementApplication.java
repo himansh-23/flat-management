f83f67737c66be17d4332f048a11b9648eff8fd5
Himanshu Prajapati (NCS) <himanshu.prajapati@in.ncs-i.com>
2023-07-05 14:35:08 +0530

    First Commit

---
+++ src/main/java/com/flat/management/FlatManagementApplication.java
@@ -0,0 +1,13 @@
+package com.flat.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flat/management/FlatManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flat.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
